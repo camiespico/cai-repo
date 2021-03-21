e907c9f6902ff4462b0fdc3266e37cbc9f20067b
Camilo Espinosa <camilo.espinosa@comafi.com.ar>
2021-03-21 19:36:03 -0300

    Primer ejercicio de encapsulamiento

---
+++ Concesionario/Concesionario.Consola/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Concesionario.Libreria.Entidades.Agencia;
+
+namespace Concesionario.Consola
+{
+    class Agencia
+    {
+        static void Main(string[] args)
+        {
+
+            Auto f = new Auto();
+
+            f.Id = 1;
+            f.Marca = "Ford";
+            f.Modelo = "Ka";
+            f.Precio = "1200000";
+
+            Console.WriteLine("El Auto " + f.Id + " de marca " + f.Marca + " tiene un precio de " + f.Precio);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Concesionario/Concesionario.Libreria/Entidades/Agencia/Auto.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concesionario.Libreria.Entidades.Agencia
+{
+    public class Auto
+    {
+        private int id;
+        private string marca;
+        private string modelo;
+        private int km;
+        private double precio;
+
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                this.id = value;
+            }
+        }
+        public string Marca
+        {
+            get
+            {
+                return this.marca;
+            }
+            set
+            {
+                this.marca = value;
+            }
+        }
+        public string Modelo
+        {
+            get
+            {
+                return this.modelo;
+            }
+            set
+            {
+                this.modelo = value;
+            }
+        }
+        public string Km
+        {
+            get
+            {
+                return this.km.ToString() + " km";
+            }
+            set
+            {
+                this.km = Convert.ToInt32(value);
+            }
+        }
+        public string Precio
+        {
+            get
+            {
+                return "$" + this.precio.ToString() + " pesos";
+            }
+            set
+            {
+                precio = Convert.ToDouble(value);
+            }
+        }
+    }
+}
